ce092fa11f256c75e439aad235b3ea7da2a880fc
lorena <dlm.lorena@gmail.com>
2022-09-14 20:37:45 -0300

    consumo cadastro e login

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Usuario } from '../model/Usuario';
+import { UsuarioLogin } from '../model/UsuarioLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
+
+  entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin>
+  {
+    return this.http.post<UsuarioLogin>(`http://localhost:8080/usuarios/logar`, usuarioLogin)
+  }
+
+  cadastrar(usuario: Usuario): Observable<Usuario>
+  {
+    return this.http.post<Usuario>(`http://localhost:8080/usuarios/cadastrar`, usuario)
+  }
+
+}
