182bb8ed3f6bb0cde36b27d5991706d68f6e596e
lorena <dlm.lorena@gmail.com>
2022-09-14 16:28:24 -0300

    menu e rodapé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,19 @@ import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-//import { MenuComponent } from './menu/menu.component';
-//import { RodapeComponent } from './rodape/rodape.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 //import { EntrarComponent } from './entrar/entrar.component';
 //import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { FormsModule } from '@angular/forms';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    //MenuComponent,
- //   RodapeComponent,
+    MenuComponent,
+    RodapeComponent,
   //  EntrarComponent,
   //  CadastrarComponent
   ],
