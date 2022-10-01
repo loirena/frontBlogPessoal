f6bd8ed648d4ac6924508d9b32a6f86649855aa8
lorena <dlm.lorena@gmail.com>
2022-09-30 23:57:55 -0300

    amelie

--- src/app/model/Postagem.ts
+++ src/app/model/Postagem.ts
@@ -4,10 +4,10 @@ import { Usuario } from "./Usuario"
 export class Postagem
 {
     public id: number
+    public foto: string
     public titulo: string
     public texto: string
     public data: Date
     public usuario: Usuario
     public tema: Tema
-    public foto: string
 }
