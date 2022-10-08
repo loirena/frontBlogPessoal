7a0f178976390fbefdc750aa74b8e9c7b220bc5b
lorena <dlm.lorena@gmail.com>
2022-10-07 23:53:00 -0300

    order by

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -26,6 +26,9 @@ export class InicioComponent implements OnInit {
   usuario: Usuario = new Usuario()
   idUser = environment.id
 
+  key: 'data'
+  reverse = true
+
   constructor(
     private router: Router,
     private postagemService: PostagemService,
